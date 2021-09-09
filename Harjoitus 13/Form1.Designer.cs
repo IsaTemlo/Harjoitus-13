f3f6406909ea9621398aa05813a6314319e2428b
Temlo <89012075+IsaTemlo@users.noreply.github.com>
2021-09-10 00:25:16 +0300

    Add project files.

---
+++ Harjoitus 13/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace Harjoitus_13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TarkastaBT = new System.Windows.Forms.Button();
+            this.NimiTB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.VastausLB = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TarkastaBT
+            // 
+            this.TarkastaBT.Location = new System.Drawing.Point(712, 261);
+            this.TarkastaBT.Name = "TarkastaBT";
+            this.TarkastaBT.Size = new System.Drawing.Size(202, 66);
+            this.TarkastaBT.TabIndex = 0;
+            this.TarkastaBT.Text = "Tarkasta";
+            this.TarkastaBT.UseVisualStyleBackColor = true;
+            this.TarkastaBT.Click += new System.EventHandler(this.TarkastaBT_Click);
+            // 
+            // NimiTB
+            // 
+            this.NimiTB.Location = new System.Drawing.Point(712, 123);
+            this.NimiTB.Name = "NimiTB";
+            this.NimiTB.Size = new System.Drawing.Size(202, 44);
+            this.NimiTB.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 123);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(693, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Anna nimesi, niin tarkastan, onko se 50 suosittuimman joukossa:";
+            // 
+            // VastausLB
+            // 
+            this.VastausLB.AutoSize = true;
+            this.VastausLB.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.VastausLB.Location = new System.Drawing.Point(18, 261);
+            this.VastausLB.Name = "VastausLB";
+            this.VastausLB.Size = new System.Drawing.Size(93, 29);
+            this.VastausLB.TabIndex = 3;
+            this.VastausLB.Text = "vastaus";
+            this.VastausLB.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(987, 542);
+            this.Controls.Add(this.VastausLB);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.NimiTB);
+            this.Controls.Add(this.TarkastaBT);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button TarkastaBT;
+        private System.Windows.Forms.TextBox NimiTB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label VastausLB;
+    }
+}
+
